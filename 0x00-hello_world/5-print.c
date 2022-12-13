ab79f03243f61b755a4f79a1c57bc129523e0dc2
growthMyndset <ufakekereuwak2021@gmail.com>
2022-12-12 21:04:39 -0800

    5 print commit

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Return: Always 0 (success)
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 
 {
-	printf("\"with proper grammar,but the outcome is a piece of art,");
+	printf("with proper grammar,but the outcome is a piece of art,\n");
 	return (0);
 }
