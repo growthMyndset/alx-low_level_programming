27879edb17b544c16e83cbd524a745d5c88e8bc3
growthMyndset <ufakekereuwak2021@gmail.com>
2022-12-14 01:53:06 -0800

    positive commit o

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,28 +5,47 @@
 /**
  * main - main function to generate a random number
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  *
  */
 
 int main(void)
-{  
-        int n;
+{
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
-       	{
-		printf("%d is positive\n", n);
-        }
+	{
+												printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-        {
+	{
 		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
-        {
+	{
 		printf("%d is negative\n", n);
-        }
-        return (0);
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
