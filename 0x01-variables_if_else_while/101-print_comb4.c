195362331b10444a893af60c96feee0f7d4c0bfd
growthMyndset <ufakekereuwak2021@gmail.com>
2022-12-16 04:43:40 -0800

    101 print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,20 +12,19 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 	int hundreds = '0';
-	
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if(!((ones == tens) || (tens == hundreds) ||tens)) /*eliminates repitition*/
+				if(!((ones == tens) || (tens == hundreds) || tens))/*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
 
-					if (!(ones == '9' && hundreds == '7' && tens == '8')) /*adds commas and spaces*/
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))/*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
@@ -35,6 +34,7 @@ int main(void)
 	       	}
        	}
 	putchar('\n');
+
 	return (0);
 }
 
