72107942e840107e3dc0006f218f0c0b0a4ee9c7
growthMyndset <ufakekereuwak2021@gmail.com>
2022-12-16 01:13:37 -0800

    9 print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - main function prints all possible combinations of single-digit numbers
+ * main -  prints all possible combinations of single-digit numbers
  *
  * Return: Always 0 (Success)
  *
